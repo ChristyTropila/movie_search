07e3494d38acedbba01a62bcdc7a29068f86176a
ChristyTropila <tropila04@gmail.com>
2020-11-25 19:42:35 -0500

    hide api

--- src/requests.js
+++ src/requests.js
@@ -1,11 +1,14 @@
-  const API_KEY="675858618060cf82c61a70ebd38cf882"
+require('dotenv').config();
+ const api_key= process.env.REACT_APP_API;
+
+// console.log(api_key)
 
   const requests={
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&page=1`,
-    fetchTrending1: `/trending/all/week?api_key=${API_KEY}&page=2`,
-    fetchTrending2: `/trending/all/week?api_key=${API_KEY}&page=3`,
-    fetchTrending3: `/trending/all/week?api_key=${API_KEY}&page=4`,
-    fetchTrending4: `/trending/all/week?api_key=${API_KEY}&page=5`,
+    fetchTrending: `/trending/all/week?api_key=${api_key}&page=1`,
+    fetchTrending1: `/trending/all/week?api_key=${api_key}&page=2`,
+    fetchTrending2: `/trending/all/week?api_key=${api_key}&page=3`,
+    fetchTrending3: `/trending/all/week?api_key=${api_key}&page=4`,
+    fetchTrending4: `/trending/all/week?api_key=${api_key}&page=5`,
     // fetchTrending5: `/trending/all/week?api_key=${API_KEY}&page=6`,
     // fetchTrending6: `/trending/all/week?api_key=${API_KEY}&page=7`,
     // fetchTrending7: `/trending/all/week?api_key=${API_KEY}&page=8`,
